b98575359054e238352435c9fdbb3a712cb969a6
大目竣 <tatsurudaime@oomeshunnomacbook-pro.local>
2020-01-15 22:40:12 +0900

    Initial Commit

---
+++ ｌｌ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ｌｌ
+//
+//  Created by 大目竣 on 2020/01/15.
+//  Copyright © 2020 大目竣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
