b1391342a78c36e7da5b0ff73165e9f73550a98c
yuuyake9191 <yuuyake9191@yahoo.co.jp>
2020-01-17 08:48:24 +0900

    say hello

--- ｌｌ/ViewController.swift
+++ ｌｌ/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func sayhello(_ sender: Any) {
+        label.text = "こんにちは"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
